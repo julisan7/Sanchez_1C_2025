2a97e3d108889db447ed2022300cb966f2a0f702
Julieta Sanchez <julieta.sanchez@ingenieria.uner.edu.ar>
2025-05-23 16:07:50 -0300

    Comienzo con el documento del proyecto integrador

---
+++ firmware/projects/Proyecto_Integrador/main/Proyecto_Integrador.c
@@ -0,0 +1,41 @@
+/*! @mainpage Proyecto_Integrador
+ *
+ * @section genDesc General Description
+ *
+ * Mi proyecto consiste en sensar el nivel de iluminación interior y la iluminación exterior para abrir las cortinas o
+ * prender las luces según corresponda. El circuito tendrá conectividad a una computadora para fijar los horarios
+ * en los que debe funcionar e informar horas de encendido y los umbrales detectados en ese momento.
+ *
+ * <a href="https://drive.google.com/...">Operation Example</a>
+ *
+ * @section hardConn Hardware Connection
+ *
+ * |    Peripheral  |   ESP32   	|
+ * |:--------------:|:--------------|
+ * | 	PIN_X	 	| 	GPIO_X		|
+ *
+ *
+ * @section changelog Changelog
+ *
+ * |   Date	    | Description                                    |
+ * |:----------:|:-----------------------------------------------|
+ * | 23/05/2025 | Creacion del documento                         |
+ *
+ * @author Julieta Sanchez (julieta.sanchez@ingenieria.uner.edu.ar)
+ *
+ */
+
+/*==================[inclusions]=============================================*/
+#include <stdio.h>
+#include <stdint.h>
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[external functions definition]==========================*/
+void app_main(void){
+	printf("Hello world!\n");
+}
+/*==================[end of file]============================================*/
