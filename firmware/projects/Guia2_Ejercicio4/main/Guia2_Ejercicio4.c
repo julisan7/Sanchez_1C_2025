1578806b6baedc28fb2b5387963b1136eaa4d2c4
Julieta Sanchez <julieta.sanchez@ingenieria.uner.edu.ar>
2025-05-23 13:54:49 -0300

    Se adjuta el screenshot del osciloscopio para la actividad 4.

--- firmware/projects/Guia2_Ejercicio4/main/Guia2_Ejercicio4.c
+++ firmware/projects/Guia2_Ejercicio4/main/Guia2_Ejercicio4.c
@@ -17,9 +17,10 @@
  *
  * @section hardConn Hardware Connection
  *
- * |    Peripheral  |   ESP32   	|
- * |:--------------:|:--------------|
- * | 	PIN_X	 	| 	GPIO_X		|
+ * |    Peripheral          |   ESP32   	|
+ * |:----------------------:|:--------------|
+ * | Entrada analogica	 	| 	CH1  		|
+ * | Salida analogica	 	| 	CH0  		|
  *
  *
  * @section changelog Changelog
@@ -54,7 +55,7 @@ TaskHandle_t GenerarECG_task_handle = NULL;
 
 uint16_t datosConvertidos;//son 12 bits pero no hay de 12 asi que uso 16 pq 8 es poco
 
-const char ecg[BUFFER_SIZE] = {
+/*const char ecg[BUFFER_SIZE] = {
     76, 77, 78, 77, 79, 86, 81, 76, 84, 93, 85, 80,
     89, 95, 89, 85, 93, 98, 94, 88, 98, 105, 96, 91,
     99, 105, 101, 96, 102, 106, 101, 96, 100, 107, 101,
@@ -72,8 +73,17 @@ const char ecg[BUFFER_SIZE] = {
     100, 106, 101, 93, 101, 105, 103, 96, 105, 112, 105, 99, 103, 108,
     99, 96, 102, 106, 99, 90, 92, 100, 87, 80, 82, 88, 77, 69, 75, 79,
     74, 67, 71, 78, 72, 67, 73, 81, 77, 71, 75, 84, 79, 77, 77, 76, 76,
-};
+};*/
+const char ecg[BUFFER_SIZE] = {
+17,17,17,17,17,17,17,17,17,17,17,18,18,18,17,17,17,17,17,17,17,18,18,18,18,18,18,18,17,17,16,16,16,16,17,17,18,18,18,17,17,17,17,
+18,18,19,21,22,24,25,26,27,28,29,31,32,33,34,34,35,37,38,37,34,29,24,19,15,14,15,16,17,17,17,16,15,14,13,13,13,13,13,13,13,12,12,
+10,6,2,3,15,43,88,145,199,237,252,242,211,167,117,70,35,16,14,22,32,38,37,32,27,24,24,26,27,28,28,27,28,28,30,31,31,31,32,33,34,36,
+38,39,40,41,42,43,45,47,49,51,53,55,57,60,62,65,68,71,75,79,83,87,92,97,101,106,111,116,121,125,129,133,136,138,139,140,140,139,137,
+133,129,123,117,109,101,92,84,77,70,64,58,52,47,42,39,36,34,31,30,28,27,26,25,25,25,25,25,25,25,25,24,24,24,24,25,25,25,25,25,25,25,
+24,24,24,24,24,24,24,24,23,23,22,22,21,21,21,20,20,20,20,20,19,19,18,18,18,19,19,19,19,18,17,17,18,18,18,18,18,18,18,18,17,17,17,17,
+17,17,17
 
+} ;
 /*==================[internal functions declaration]=========================*/
 /*
 * @brief Funcion invocada para leer y enviar los datos
@@ -92,9 +102,9 @@ static void TareaLeerEnviar(void *pvParameter){ //convierte un dato de analogico
 * @brief Esta funcion pasa la señal del buffer a analogico
 */
 static void TareaGenerarECG(void *pvParameter){
-	ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 	int i=0;
 	while(true){
+		ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 		AnalogOutputWrite(ecg[i]);
 		i++;
 		if (i == BUFFER_SIZE){
@@ -106,6 +116,7 @@ static void TareaGenerarECG(void *pvParameter){
 void FuncTimerTareas(void *param){
 	vTaskNotifyGiveFromISR(LeerEnviar_task_handle, pdFALSE);	  /* Envía una notificación a la tarea asociada a Leer y medir*/
 	vTaskNotifyGiveFromISR(GenerarECG_task_handle, pdFALSE);
+
 }
 
 /*==================[external functions definition]==========================*/
